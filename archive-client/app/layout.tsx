979d59da6c65b51c449cb0c4f38919214768b6f9
geomachine <javascriptiqbal@gmail.com>
2026-02-07 16:22:19 +0600

    add krakens analytics

--- archive-client/app/layout.tsx
+++ archive-client/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme/theme-provider";
 
@@ -28,6 +29,15 @@ export default function RootLayout({
             <body
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
             >
+                {/* Krakens Analytics */}
+                <Script
+                    src="https://krakens.nesohq.org/krakens.js"
+                    strategy="afterInteractive"
+                />
+                <Script id="krakens-init" strategy="afterInteractive">
+                    {`Krakens.init('hrd_6f7e57b8d05b26e2f0801e0e0bd6437d5295eefbf93589defcb04df743f4a776');`}
+                </Script>
+
                 <ThemeProvider
                     attribute="class"
                     defaultTheme="system"
