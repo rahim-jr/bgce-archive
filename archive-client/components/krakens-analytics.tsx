13d6738da0c099d5d085c3722cea1133ad27f919
geomachine <javascriptiqbal@gmail.com>
2026-02-07 16:50:23 +0600

    add krakens analytics: new implementations

--- archive-client/components/krakens-analytics.tsx
+++ archive-client/components/krakens-analytics.tsx
@@ -4,14 +4,15 @@ import Script from 'next/script';
 
 export function KrakensAnalytics() {
     return (
-        <Script
-            src="https://krakens.nesohq.org/krakens.js"
-            strategy="afterInteractive"
-            onLoad={() => {
-                if (typeof window !== 'undefined' && (window as any).Krakens) {
-                    (window as any).Krakens.init('hrd_6f7e57b8d05b26e2f0801e0e0bd6437d5295eefbf93589defcb04df743f4a776');
-                }
-            }}
-        />
+        <>
+            {/* Krakens Analytics */}
+            <Script
+                src="https://krakens.nesohq.org/krakens.js"
+                strategy="afterInteractive"
+            />
+            <Script id="krakens-init" strategy="afterInteractive">
+                {`Krakens.init('hrd_6f7e57b8d05b26e2f0801e0e0bd6437d5295eefbf93589defcb04df743f4a776');`}
+            </Script>
+        </>
     );
 }
